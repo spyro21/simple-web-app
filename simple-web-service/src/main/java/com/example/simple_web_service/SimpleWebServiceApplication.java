0c422db64a22f108bbb75f72524aeec99c0fa6cc
Jack Legate <jacklegate21@gmail.com>
2024-09-16 14:40:32 -0600

    service init

---
+++ simple-web-service/src/main/java/com/example/simple_web_service/SimpleWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebServiceApplication.class, args);
+	}
+
+}
---
+++ simple-web-service/src/test/java/com/example/simple_web_service/SimpleWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
